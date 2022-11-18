abe43558e19fd01b9c28cc62aec0743eb8537dd9
Ashif <reachashif@outlook.com>
2022-11-18 12:08:31 +1100

    first commit

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        Console.WriteLine("Hello_World");
+            Console.Write("");
+            Console.ReadKey();
+    }
+}
